902a65956bea8b726a78dd6acb6060813d8ebe49
m <aem6236@gmail.com>
2020-03-19 14:42:10 +0200

    section 04 ex #01 simple program

--- _cpp_codes/sec04_#01.cpp
+++ _cpp_codes/sec04_#01.cpp
@@ -1,5 +1,13 @@
+
+
 #include<iostream>
 
+/****************************************************************************** 
+    main    : returns non-zero if the main is error
+    Pre     : The stack exists and it has been initialized.
+    Post    : Return non-zero if the stack is empty; zero, otherwise.
+    
+*******************************************************************************/
 int main(int argc, char *argv[]){
 
     
