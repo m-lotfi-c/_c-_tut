c6002a6e76a982cdbeadd26f70b0afbf78d9d57b
m-lotfi-c <aem6236@gmail.com>
2020-03-19 15:57:25 +0200

    section 04 challenge - solution

--- _cpp_codes/sec04_challenge.cpp
+++ _cpp_codes/sec04_challenge.cpp
@@ -6,9 +6,13 @@
     main    : returns non-zero if the main is error
     Pre     : The stack exists and it has been initialized.
     Post    : Return non-zero if the stack is empty; zero, otherwise.
-    
-*******************************************************************************/
-int main(int argc, char *argv[]){
 
-    
+*******************************************************************************/
+int main(int argc, char *argv[]){ 
+    int favorite_number;
+    std::cout << "Enter your favorite number between 1 and 100: " << std::endl;
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+
+    return 0;    
 }
