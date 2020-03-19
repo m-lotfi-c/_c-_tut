9196e9ca790fb8b45dae1fb2c03bd4b671a948d8
m-lotfi-c <aem6236@gmail.com>
2020-03-19 18:09:17 +0200

    section 04 challenge - solution

--- _cpp_codes/sec04_challenge.cpp
+++ _cpp_codes/sec04_challenge.cpp
@@ -13,6 +13,6 @@ int main(int argc, char *argv[]){
     std::cout << "Enter your favorite number between 1 and 100: " << std::endl;
     std::cin >> favorite_number;
     std::cout << "Amazing!! That's my favorite number too!" << std::endl;
-
+    std::cout << "No really!! " << favorite_number << " is my favorite number! " << std::endl;
     return 0;    
 }
